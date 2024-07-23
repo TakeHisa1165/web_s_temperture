f9bcd124e42775d4339d84bddda3d8bfc33d690c
TakeHisa1165 <harugen2963@gmail.com>
2024-07-23 09:15:15 +0900

    new web_s_temperture

--- gui.py
+++ gui.py
@@ -1,59 +1,108 @@
-import PySimpleGUI as sg
-import w_csv
+import csv
+import flet as ft
 
 
-class Select_File:
-    def __init__(self):
-        self.path_dict = self.select_file()
+def main(page: ft.Page):
+    # 温度計ファイル選択のコールバック関数
+    def pick_temperature_file_result(e: ft.FilePickerResultEvent):
+        print(e.path)
+        if e.files:
+            temperature_file_path.value = e.files[0].path
+            temperature_file_path.update()
 
-    def select_file(self):
+    # CSVファイル選択のコールバック関数
+    def pick_csv_file_result(e: ft.FilePickerResultEvent):
+        if e:
+            csv_file_paths.value = e.path
+            csv_file_paths.update()
 
-        sg.theme('SystemDefault')
+    # Excelファイル選択のコールバック関数
+    def pick_excel_file_result(e: ft.FilePickerResultEvent):
+        if e.files:
+            excel_file_path.value = e.files[0].path
+            excel_file_path.update()
 
-        layout = [
-            [sg.Text('データロガー内の Records.html を選択してください', size=(50, 1), font=('メイリオ', 14))],
-            [sg.InputText(font=('メイリオ', 14)), sg.FilesBrowse('開く', key='File1', font=('メイリオ', 14))],
-            [sg.Text('CSVファイルのダウンロード先フォルダを選んでください', size=(50, 1), font=('メイリオ', 14))],
-            [sg.InputText(font=('メイリオ', 14)), sg.FolderBrowse('開く', font=('メイリオ', 14))],
-            [sg.Text('chromedriverの場所を指定してください', size=(50, 1), font=('メイリオ', 14))],
-            [sg.InputText(font=('メイリオ', 14)), sg.FilesBrowse('開く', key='File1', font=('メイリオ', 14))],
-            [sg.Text('記録用Excelファイルを選択してください', size=(50, 1), font=('メイリオ', 14))],
-            [sg.InputText(font=('メイリオ', 14)), sg.FilesBrowse('開く', key='File1', font=('メイリオ', 14))],
-            [sg.Submit(button_text='設定', font=('メイリオ', 14)), sg.Submit(button_text="閉じる", font=('メイリオ', 14))],
-        ]
+    # Chrome Driverファイル選択のコールバック関数
+    def pick_driver_file_result(e: ft.FilePickerResultEvent):
+        if e.files:
+            driver_path.value = e.files[0].path
+            driver_path.update()
 
-        # セクション 2 - ウィンドウの生成z
-        window = sg.Window('ファイル選択', layout)
+    # CSVに登録する処理
+    def regist_to_csv(self):
+        temperature = temperature_file_path.value
+        csv_path = csv_file_paths.value
+        excel_path = excel_file_path.value
+        driver_path_val = driver_path.value
+        with open("path.csv", mode="w", newline="", encoding="UTF-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([temperature, csv_path, excel_path, driver_path_val])
 
-        # セクション 3 - イベントループ
-        while True:
-            event, values = window.read()
+    # ファイルピッカーのインスタンスを作成
+    pick_temperature_file_dialog = ft.FilePicker(on_result=pick_temperature_file_result)
+    pick_csv_file_dialog = ft.FilePicker(on_result=pick_csv_file_result)
+    pick_excel_file_dialog = ft.FilePicker(on_result=pick_excel_file_result)
+    pick_driver_file_dialog = ft.FilePicker(on_result=pick_driver_file_result)
 
-            if event is None:
-                print('exit')
-                break
+    # ファイルパス表示用のテキストコンポーネントを作成
+    temperature_file_path = ft.TextField(read_only=True, key="temperature_file_path")
+    csv_file_paths = ft.TextField(read_only=True, key="csv_file_paths")
+    excel_file_path = ft.TextField(read_only=True, key="excel_file_path")
+    driver_path = ft.TextField(read_only=True, key="driver_path")
 
-            if event == '設定':
-                path_dict = {}
-                logger_path = values[0]
-                path_dict["logger_path"] = logger_path
-                download_path = values[1]
-                path_dict["download_path"] = download_path
-                driver_path = values[2]
-                path_dict["driver_path"] = driver_path
-                excel_path = values[3]
-                path_dict["excel_path"] = excel_path
-                csv = w_csv.Write_csv()
-                csv.write_csv(path_dict=path_dict)
+    # ページにファイルピッカーを追加
+    page.overlay.extend([pick_temperature_file_dialog, pick_csv_file_dialog, pick_excel_file_dialog, pick_driver_file_dialog])
 
-                return path_dict
+    # ページにUIコンポーネントを追加
+    page.add(
+        ft.Row(
+            [
+                ft.Text("Htmlを選択してください"),
+                ft.ElevatedButton(
+                    "Html",
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=lambda _: pick_temperature_file_dialog.pick_files(),
+                ),
+                temperature_file_path,
+            ]
+        ),
+        ft.Row(
+            [
+                ft.Text("ダウンロードフォルダを選択してください"),
+                ft.ElevatedButton(
+                    "フォルダ",
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=lambda _: pick_csv_file_dialog.get_directory_path(),
+                ),
+                csv_file_paths,
+            ]
+        ),
+        ft.Row(
+            [
+                ft.Text("書き出すExcelファイルを選択してください。"),
+                ft.ElevatedButton(
+                    "Excel",
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=lambda _: pick_excel_file_dialog.pick_files(allow_multiple=True),
+                ),
+                excel_file_path,
+            ]
+        ),
+        ft.Row(
+            [
+                ft.Text("Chrome Driverのファイルを選択してください"),
+                ft.ElevatedButton(
+                    "Chrome Driver",
+                    icon=ft.icons.FILE_OPEN,
+                    on_click=lambda _: pick_driver_file_dialog.pick_files(allow_multiple=True),
+                ),
+                driver_path,
+            ]
+        ),
+    )
 
-            if event == '閉じる':
-                break
-
-        #  セクション 4 - ウィンドウの破棄と終了
-        window.close()
+    # 登録ボタンを追加
+    page.add(ft.ElevatedButton(text="登録", on_click=regist_to_csv))
 
 
-if __name__ == "__main__":
-    Select_File()
+ft.app(target=main)
--- main.py
+++ main.py
@@ -1,23 +1,76 @@
 """
-温度データ記録用、メイン制御プログラム
+A&D AD-5326T を使用した気温記録アプリケーション
 """
+import csv
+import os
+import xlwings as xw
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+from selenium.webdriver.common.by import By
+import tkinter.messagebox as tmsg
+import sys
 
-import r_csv
-import w_csv
-import gui
-import download_file
-import edit_data
-import to_excel
 
-# path.csvを読み込んで、必要な絶対pathを取得
-# r_csvのインスタンス
-rcsv = r_csv.Read_csv()
-logger_path, download_path, driver_path, excel_path = rcsv.read_csv()
+file = os.path.isfile("path.csv")
+if file:
+    with open("path.csv", mode="r", newline="", encoding="UTF-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            html = row[0]
+            download_dir = row[1]
+            excel = row[2]
+            driver = row[3]
+else:
+    import gui
+html_file = os.path.isfile(html)
+# if html_file:
+#     pass
+# else:
+#     tmsg.showinfo("接続確認", "温度計が接続されていません。\n温度計を接続してください。")
+#     sys.exit()
 
-# seleniumを使用してブラウザで表示されるデータのCSVを保存
-dlf = download_file.DownLoad()
-dlf.down_load(logger_paht=logger_path, download_path=download_path, driver_path=driver_path)
+driver = webdriver.Chrome()
+driver.get(html)
+driver.find_element(By.ID, "ex").click()
+time.sleep(2)
+driver.quit()
 
-# ダウンロードしたcsvファイルを編集
-edt = edit_data.EditData()
-data_frame = edt.edit_data(download_path=download_path, excel_path=excel_path)
+with open(download_dir + "/export.csv", mode="r", newline="", encoding="UTF-8") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[1])
+        if row[1] == "ID:0001":
+            sheet_name = "検査場"
+        if row[1] == "ID:0002":
+            sheet_name = 2
+        break
+wb = xw.Book(excel)
+ws = wb.sheets[sheet_name]
+
+max_row = ws.range(1000000, 1).end('up').row
+start_row = max_row + 1
+with open(download_dir + "/export.csv", mode="r", newline="", encoding="UTF-8") as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    header = next(reader)
+    header = next(reader)
+    header = next(reader)
+    # print(reader)
+    for row in reader:
+        date = row[0]
+        time = row[1]
+        temp = row[2]
+        print(date, time, temp)
+        ws.range(start_row, 1).value = date
+        ws.range(start_row, 2).value = time
+
+        if 12 <= float(temp) <= 35:
+            ws.range(start_row, 3).value = temp
+        else:
+            ws.range(start_row, 3).value = temp
+            ws.range(start_row, 3).color = "e49edd"
+        start_row += 1
+
+os.remove(download_dir + "/export.csv")
+tmsg.showinfo("完了", "処理が終わりました。\nExcelファイルを確認してください。")
